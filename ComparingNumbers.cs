5ed2bb8bfc0cb8e609fe8c1c3184fbeec68bd907
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-06-16 14:49:58 +0300

    Add files via upload

---
+++ ComparingNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine("Enter two numbers.");
+Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+int biggerNumber = firstNumber;
+if (secondNumber > firstNumber) { 
+    biggerNumber = secondNumber;
+}
+Console.WriteLine("The bigger number is: {0}", biggerNumber);
