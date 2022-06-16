cedad82d8a090d2ca66796d746ac2aeeabd8931b
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-06-16 17:45:10 +0300

    Add files via upload

---
+++ ConcatenatingStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class FloatingDataTypes
+{
+    static void Main(String[] args)
+    {
+        string firstName = "Maria";
+        string lastName = "Todorova";
+        string fullName = firstName + " " + lastName;
+
+        Console.WriteLine("Hello, " + fullName + "!");
+        Console.WriteLine("Your full name is " + fullName + ".");
+
+    }
+}
