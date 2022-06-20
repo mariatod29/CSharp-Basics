40cab7bf15884d81b9ccb5836a57c5f9709f8f48
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-06-20 12:18:43 +0300

    First Exercises with Arrays

---
+++ Arrays/ArrayReverseExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ArrayReverse
+{
+    public void ReversedArray()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int length = array.Length;
+        int[] reversed = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            reversed[length - i - 1] = array[i];
+        }
+        for(int i = 0; i < length; i++)
+        {
+            Console.Write(reversed[i] + " ");
+        }
+    }
+}
---
+++ Arrays/Arrays.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class ArrayExamples
+{
+    public void ArrayEx()
+    {
+
+    }
+}
