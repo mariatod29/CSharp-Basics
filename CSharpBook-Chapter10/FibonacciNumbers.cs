ef9726e1895bb7de9fbf4e894ceeb8019f51bb81
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-06-24 17:10:37 +0300

    Chapter 10 Pt.1

---
+++ CSharpBook-Chapter10/FibonacciNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Fibonacci
+{
+    public static long Fib(int n)
+    {
+        if(n <= 2)
+        {
+            return 1;
+        }
+        return Fib(n - 1) + Fib(n - 2);
+    }
+}
---
+++ CSharpBook-Chapter10/RecursiveFactorial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Recursion
+{
+    public static decimal Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        return n * Factorial(n - 1);
+    }
+}
