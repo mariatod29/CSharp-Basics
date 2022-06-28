a553cb0050a6d3e25d301bdec347600735bec81d
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-06-28 14:22:05 +0300

    Chapter 12 Pt.1

---
+++ CSharpBook-Chapter12/Chapter12Exercise1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Chapter12Ex1
+{
+    public static void ReadFile(string filename)
+    {
+        try
+        {
+            TextReader reader = new StreamReader(filename);
+            string line = reader.ReadLine();
+            Console.WriteLine(line);
+            reader.Close();
+        }
+        catch (FileNotFoundException fnfe)
+        {
+            Console.WriteLine("The file '{0}' is not found.", filename);
+        }
+        catch (IOException ioe)
+        {
+            Console.WriteLine(ioe.StackTrace);
+        }
+    }
+}
