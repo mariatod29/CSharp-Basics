a6b0cd029b28c3321736710c12d99465849439e5
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-05 16:16:26 +0300

    Add files via upload

---
+++ CSharpBook-Chapter15/Chapter15-Exercise11/Chapter15Exercise11.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class DeleteWordsStartingWithTest
+{
+    public static void Main()
+    {
+        string inputFile = @"wordsWithTest.txt";
+        string outputFile = @"wordsWithTestOutput.txt";
+        string backupFile = @"wordswithTestBackup.txt";
+        Console.Write("Insert the prefix: ");
+        string prefix = Console.ReadLine();
+        StreamReader reader;
+        StreamWriter writer;
+        try
+        {
+            reader = new StreamReader(inputFile, Encoding.GetEncoding("UTF-8"));
+            using (reader)
+            {
+                if (File.Exists(outputFile)) File.Delete(outputFile);
+                writer = new StreamWriter(outputFile, true, Encoding.GetEncoding("UTF-8"));
+                using (writer)
+                {
+                    for (string line; (line = reader.ReadLine()) != null;)
+                    {
+                        writer.WriteLine(Regex.Replace(line, @"\b" + prefix + @"[0-9a-zA-Z_-_]*", "", RegexOptions.IgnoreCase), true);
+                    }
+                }
+            }
+            File.Replace(outputFile, inputFile, backupFile);
+            Console.WriteLine("The prefix is successfully removed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error during deleting has occured: " + ex.Message);
+        }
+    }
+}
