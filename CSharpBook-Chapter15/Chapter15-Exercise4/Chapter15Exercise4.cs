849fe8dc61d4f65978d64c9fdc9eddcccf0ea541
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-05 16:12:47 +0300

    Add files via upload

---
+++ CSharpBook-Chapter15/Chapter15-Exercise4/Chapter15Exercise4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class LineCompare
+{
+    public static void Main()
+    {
+        string oddlines = @"oddlines.txt";
+        string morelines = @"morelines.txt";
+        StreamReader reader1, reader2;
+        int different = 0;
+        int equal = 0;
+
+        try
+        {
+            reader1 = new StreamReader(oddlines, Encoding.GetEncoding("UTF-8"));
+            reader2 = new StreamReader(morelines, Encoding.GetEncoding("UTF-8"));
+
+            using (reader1)
+            {
+                using (reader2)
+                {
+                    while (true)
+                    {
+                        string line;
+                        line = reader1.ReadLine();
+                        if (line == null) break;
+                        if (line.CompareTo(reader2.ReadLine()) == 0) equal++;
+                        else different++;
+                    }
+                }
+            }
+            Console.WriteLine("There are {0} equal and {1} different lines.", equal, different);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error has occured!" + ex.Message);
+        }
+    }
+}
