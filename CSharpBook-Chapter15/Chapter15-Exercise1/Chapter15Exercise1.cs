5719094117cc82ff591d25d8403878081bbe7c56
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-05 16:09:27 +0300

    Add files via upload

---
+++ CSharpBook-Chapter15/Chapter15-Exercise1/Chapter15Exercise1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+class OddLines
+{
+    static void Main()
+    {
+        const string inputFile = "oddlines.txt";
+        StreamReader reader = new StreamReader(inputFile);
+
+        try
+        {
+            reader = new StreamReader(inputFile, Encoding.GetEncoding("UTF-8"));
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    reader.ReadLine();
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An exception occured!" + ex.Message);
+        }
+    }
+}
