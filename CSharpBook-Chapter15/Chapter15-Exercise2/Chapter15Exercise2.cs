1ead8da078a40b16a082a1d61b6a9cb36a4c724a
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-05 16:10:20 +0300

    Add files via upload

---
+++ CSharpBook-Chapter15/Chapter15-Exercise2/Chapter15Exercise2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class ConcatenateTwoFiles
+{
+    static void Main()
+    {
+        string oddLinesFile = @"oddlines.txt";
+        string moreLinesFile = @"morelines.txt";
+        string concatenated = @"mergedfiles.txt";
+        StreamReader reader1, reader2;
+        StreamWriter writer;
+
+        try
+        {
+            reader1 = new StreamReader(oddLinesFile, Encoding.GetEncoding("UTF-8"));
+            reader2 = new StreamReader(moreLinesFile, Encoding.GetEncoding("UTF-8"));
+            writer = new StreamWriter(concatenated, false, Encoding.GetEncoding("UTF-8"));
+
+            using (reader1)
+            {
+                using (reader2)
+                {
+                    using (writer)
+                    {
+                        writer.WriteLine(reader1.ReadToEnd() + "\n" + reader2.ReadToEnd());
+                    }
+                }
+            }
+            Console.WriteLine("The files are merged.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error has occured!", ex.Message);
+        }
+    }
+}
