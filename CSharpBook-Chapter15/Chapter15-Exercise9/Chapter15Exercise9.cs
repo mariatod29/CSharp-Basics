0f87196b086f286396d239e08d422cfe790d9dfb
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-05 16:15:24 +0300

    Add files via upload

---
+++ CSharpBook-Chapter15/Chapter15-Exercise9/Chapter15Exercise9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class DeleteOddLinesClass
+{
+
+    static void Main(string[] args)
+    {
+        string inputFile = @"deletingOddLines.txt";
+        string outputFile = @"deletedLines.txt";
+        string backupFile = @"backup.txt";
+        StreamReader reader;
+        StreamWriter writer;
+        try
+        {
+
+            reader = new StreamReader(inputFile);
+            using (reader)
+            {
+                writer = new StreamWriter(outputFile, false);
+                using (writer)
+                {
+                    for (string line; (line = reader.ReadLine()) != null;)
+                    {
+                        writer.WriteLine(reader.ReadLine());
+                    }
+                }
+            }
+            File.Replace(outputFile, inputFile, backupFile);
+            Console.WriteLine("The odd line are deleted");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occured during operation. " + ex.Message);
+        }
+    }
+}
