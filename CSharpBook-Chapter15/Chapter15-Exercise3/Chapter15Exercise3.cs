923e7f41a1dcbb04a77d92284e03bb8c23890f38
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-05 16:11:09 +0300

    Add files via upload

---
+++ CSharpBook-Chapter15/Chapter15-Exercise3/Chapter15Exercise3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+class LineNumbers
+{
+    public static void Main()
+    {
+        string readFromFile = @"mergedfiles.txt";
+        string writeIntoFile = @"numberedtxt.txt";
+        StreamReader reader;
+        StreamWriter writer, writerFirstLine;
+
+        try
+        {
+            reader = new StreamReader(readFromFile, Encoding.GetEncoding("UTF-8"));
+            using (reader)
+            {
+                writerFirstLine = new StreamWriter(writeIntoFile, false, Encoding.GetEncoding("UTF-8"));
+                using (writerFirstLine)
+                {
+                    writerFirstLine.WriteLine(("1: " + reader.ReadLine()));
+                }
+                writer = new StreamWriter(writeIntoFile, true, Encoding.GetEncoding("UTF-8"));
+                using (writer)
+                {
+                    string line = reader.ReadLine();
+                    int i = 2;
+
+                    while (line != null)
+                    {
+                        writer.WriteLine(String.Format("{0}: ", i) + line);
+                        line = reader.ReadLine();
+                        i++;
+                    }
+                }
+
+            }
+            Console.WriteLine("The lines are numbered.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error has occurred!", ex.Message);
+        }
+    }
+}
