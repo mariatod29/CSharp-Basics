0e93060cb2c6f6f1637193c476dbcc50235b95b7
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-19 16:29:48 +0300

    Add files via upload

---
+++ XML Project/CodeListParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+[XmlRoot("Value")]
+
+//Here is the class:
+public class CodeListParser
+{
+    public string simpleValueCode { get; set; }
+    public string simpleValueName { get; set; }
+
+    public CodeListParser()
+    {
+        this.simpleValueCode = simpleValueCode;
+        this.simpleValueName = simpleValueName;
+    }
+}
+
+
---
+++ XML Project/Row.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XMLProject
+{
+    [Serializable()]
+    public class Row
+    {
+        [XmlElement("Value")]
+        public string simpleValueCode { get; set; }
+        [XmlElement("Value")]
+        public string simpleValueName { get; set; }
+    }
+}
