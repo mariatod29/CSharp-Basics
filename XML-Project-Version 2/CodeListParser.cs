d64fc99fcdea632e3d761f9661375ef67b9f094f
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-07-25 09:43:07 +0300

    Add files via upload

---
+++ XML-Project-Version 2/CodeListParser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+class CodeListParser
+{
+    internal Dictionary<string, string> GetRulePlanRsa()
+    {
+        var codeListBytes = File.ReadAllBytes(@"C:\Users\mtodorova\OneDrive - Blubito GmbH\Dokumente\Codelists\C17000608_genericode.xml");
+        return GetValuePairs(codeListBytes, "code", "name");
+    }
+
+    public Dictionary<string, string> GetLegalForms()
+    {
+        var codeListBytes = File.ReadAllBytes(@"C:\Users\mtodorova\OneDrive - Blubito GmbH\Dokumente\Codelists\C60000022_genericode.xml");
+        return GetValuePairs(codeListBytes, "Code", "Bezeichnung");
+    }
+
+
+    Dictionary<string, string> GetValuePairs(byte[] codeListBytes, string keyName, string valueName)
+    {
+        XDocument xDoc = XDocument.Load(new MemoryStream(codeListBytes));
+        XNamespace ns = "http://docs.oasis-open.org/codelist/ns/genericode/1.0/";
+        IEnumerable<XElement> rows = from row in xDoc.Descendants(ns + "Row")
+                                     select row;
+
+        var namesByCode = new Dictionary<string, string>();
+
+        foreach (var row in rows)
+        {
+            var code = row.Elements(ns + "Value").FirstOrDefault(v => (string)v.Attribute("ColumnRef") == keyName).Value ?? string.Empty;
+            var value = row.Elements(ns + "Value").FirstOrDefault(v => (string)v.Attribute("ColumnRef") == valueName).Value ?? string.Empty;
+
+            namesByCode.Add(code, value);
+        }
+
+        foreach (var item in namesByCode)
+        {
+            Console.WriteLine("Key - {0}, Value - {1}", item.Key, item.Value);
+        }
+        // namesByCode.ToList().ForEach(x => Console.WriteLine(x.Key));
+
+        return namesByCode;
+    }
+}
---
+++ XML-Project-Version 2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+CodeListParser parser = new CodeListParser();
+parser.GetRulePlanRsa();
+Console.ReadLine();
