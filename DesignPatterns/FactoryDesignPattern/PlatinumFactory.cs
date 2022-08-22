0f36009b8506bfbf4de7f1c5bdfce977423f91ba
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-08-22 15:47:19 +0300

    Add files via upload

---
+++ DesignPatterns/FactoryDesignPattern/PlatinumFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new Platinum();
+            return product;
+        }
+    }
+}
---
+++ DesignPatterns/FactoryDesignPattern/TitaniumFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new Titanium();
+            return product;
+        }
+    }
+}
