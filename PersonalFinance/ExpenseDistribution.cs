add34f2874c37e6c6343d2efcccdda4219898b82
mariatod29 <107251186+mariatod29@users.noreply.github.com>
2022-08-25 15:43:36 +0300

    Add files via upload

---
+++ PersonalFinance/ExpenseDistribution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance
+{
+    public class ExpenseDistribution
+    {
+        private string expenseDistributionName;
+        private List<ExpenseDistributionItem> expenseDistributionItems;
+
+        
+    }
+}
---
+++ PersonalFinance/ExpenseType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinance
+{
+    public class ExpenseType
+    {
+
+        public string Name 
+        { 
+            get{ return this.Name; } 
+            set 
+            { 
+                if (this.Name == null)
+                {
+                    throw new ArgumentException("The input is invalid.");
+                }
+                else
+                {
+                    this.Name = value;
+                }
+            } 
+        }
+    }
+}
